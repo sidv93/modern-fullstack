649a8a86584a5c4730c65286b443bb7b56bafe11
Siddharth Venkatesh <32375112+sidv93@users.noreply.github.com>
2020-09-19 00:06:01 +0530

    added resolvers

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
+import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import ormconfig from './mikro-orm.config';
 import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
     const orm = await MikroORM.init(ormconfig);
@@ -13,13 +15,14 @@ const main = async () => {
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver],
+            resolvers: [HelloResolver, PostResolver],
             validate: false
-        })
+        }),
+        context: () => ({ em: orm.em })
     });
 
     apolloServer.applyMiddleware({ app });
-    
+
     app.listen(4000, () => {
         console.log('Server up at localhost:4000');
     })
