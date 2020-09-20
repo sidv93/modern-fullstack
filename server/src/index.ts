607f5cbd4e6aeab200b1f9f37fafaded0aaffbd3
Siddharth Venkatesh <32375112+sidv93@users.noreply.github.com>
2020-09-21 00:03:02 +0530

    Added register mutation

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,6 +11,7 @@ import redis from 'redis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 import { __prod__ } from './constants';
+import cors from 'cors';
 
 const main = async () => {
     const orm = await MikroORM.init(ormconfig);
@@ -20,6 +21,11 @@ const main = async () => {
 
     const RedisStore = connectRedis(session);
     const redisClient = redis.createClient();
+
+    app.use(cors({
+        origin: 'http://localhost:3000',
+        credentials: true
+    }))
 
     app.use(
         session({
@@ -48,7 +54,10 @@ const main = async () => {
         context: ({ req, res }) => ({ em: orm.em, req, res })
     });
 
-    apolloServer.applyMiddleware({ app });
+    apolloServer.applyMiddleware({
+        app,
+        cors: false
+    });
 
     app.listen(4000, () => {
         console.log('Server up at localhost:4000');
--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -3,16 +3,36 @@ import { Formik, Form } from 'formik';
 import { FormControl, FormLabel, Input, FormErrorMessage, Box, Button } from '@chakra-ui/core';
 import Wrapper from '../components/wrapper';
 import Inputfield from '../components/inputfield';
+import { useMutation } from 'urql';
 
 interface registerProps {
 
 }
 
+const REGISTER_MUTATION = `
+    mutation Register($username: String!, $password: String!) {
+        register(options: { username: $username, password: $password }) {
+            errors {
+                field
+                message
+            }
+            user {
+                id
+                username
+            }
+        }
+    }
+`
+
 const Register: React.FC<registerProps> = ({ }) => {
+    const [, register] = useMutation(REGISTER_MUTATION);
     return (
         <Wrapper variant="small">
-            <Formik initialValues={{ username: '', password: '' }} onSubmit={(values) => (console.log(values))}>
-                {({isSubmitting}) => (
+            <Formik initialValues={{ username: '', password: '' }}
+                onSubmit={(values) => {
+                    return register(values);
+                }}>
+                {({ isSubmitting }) => (
                     <Form>
                         <Inputfield name="username" placeholder="username" label="Username" />
                         <Box mt={4}><Inputfield name="password" placeholder="password" label="Password" type="password" /></Box>
