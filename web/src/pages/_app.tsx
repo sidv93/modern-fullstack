384ea668dfb1229d5b8083b01f18a514599984d8
Siddharth Venkatesh <32375112+sidv93@users.noreply.github.com>
2020-09-21 01:34:11 +0530

    cache stuff

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,13 +1,61 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core'
-import { Provider, createClient } from 'urql';
-
+import { Provider, createClient, dedupExchange, fetchExchange } from 'urql';
+import { cacheExchange, QueryInput, Cache } from '@urql/exchange-graphcache';
 import theme from '../theme'
+
+function betterUpdateQuery<Result, Query>(
+    cache: Cache,
+    qi: QueryInput,
+    result: any,
+    fn: (r: Result, q: Query) => Query
+) {
+    return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
+
 
 const client = createClient({
     url: 'http://localhost:4000/graphql',
     fetchOptions: {
         credentials: 'include'
-    }
+    },
+    exchanges: [dedupExchange, cacheExchange({
+        updates: {
+            Mutation: {
+                login: (_result, args, cache, info) => {
+                    betterUpdateQuery < LoginMutation, MeQuery>(
+                        cache,
+                        { query: MeDocument },
+                        _result,
+                        (result, query) => {
+                            if (result.login.errors) {
+                                return query;
+                            } else {
+                                return {
+                                    me: result.login.user
+                                }
+                            }
+                        }
+                    )
+                },
+                register: (_result, args, cache, info) => {
+                    betterUpdateQuery <RegisterMutation, MeQuery>(
+                        cache,
+                        { query: MeDocument },
+                        _result,
+                        (result, query) => {
+                            if (result.register.errors) {
+                                return query;
+                            } else {
+                                return {
+                                    me: result.register.user
+                                }
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    }), fetchExchange]
 });
 
 function MyApp({ Component, pageProps }: any) {
@@ -15,8 +63,8 @@ function MyApp({ Component, pageProps }: any) {
         <Provider value={client}>
             <ThemeProvider theme={theme}>
                 {/* <ColorModeProvider> */}
-                    <CSSReset />
-                    <Component {...pageProps} />
+                <CSSReset />
+                <Component {...pageProps} />
                 {/* </ColorModeProvider> */}
             </ThemeProvider>
         </Provider>
